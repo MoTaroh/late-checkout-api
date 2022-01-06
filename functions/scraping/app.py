d688aaa88a031f708c499ae2fec74dd3b283d294
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-01-06 23:37:02 +0900

    Update: add limit for async

--- functions/scraping/app.py
+++ functions/scraping/app.py
@@ -11,27 +11,6 @@ from parser import Parser
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
     # TODO:eventのバリデーション -> とりあえず後回し
     print(event)
     found = event["Payload"]["found"]
@@ -52,8 +31,9 @@ def lambda_handler(event, context):
         # レイトチェックアウトホテル一覧を取得
         hotels = HOTELS
         parser = Parser()
+        parallel_limit = 10
         loop = asyncio.get_event_loop()
-        result = loop.run_until_complete(handle_request(hotels, parser, search_param))
+        result = loop.run_until_complete(handle_request(hotels, parser, search_param, parallel_limit))
 
         print("== result ==")
         print(result)
@@ -82,24 +62,26 @@ def lambda_handler(event, context):
         }
 
 
-async def get_hotel(session, url, parser, hotel):
-    r = await session.get(url)
-    html = await r.text()
+async def get_hotel(session, url: str, parser: Parser, hotel: Hotel, sem: asyncio.Semaphore):
+    async with sem:
+        r = await session.get(url)
+        html = await r.text()
+        print(hotel.hotelName)
 
-    plans = parser.parse(html)
-    hotel_result = {
-        "hotelNo": hotel.yadNo,
-        "hotelName": hotel.hotelName,
-        # "prefNo": hotel.prefNo,
-        # "regionNo": hotel.regionNo,
-        "regionName": hotel.regionName,
-        "planList": plans,
-    }
-    return hotel_result
+        plans = parser.parse(html)
+        hotel_result = {
+            "hotelNo": hotel.yadNo,
+            "hotelName": hotel.hotelName,
+            # "prefNo": hotel.prefNo,
+            # "regionNo": hotel.regionNo,
+            "regionName": hotel.regionName,
+            "planList": plans,
+        }
+        return hotel_result
 
 
-async def handle_request(hotels: dict, parser, search_param: dict):
-    print("start async")
+async def handle_request(hotels: dict, parser, search_param: dict, limit: int):
+    sem = asyncio.Semaphore(limit)
     results = []
     async with aiohttp.ClientSession() as session:
         for h in hotels:
@@ -114,8 +96,7 @@ async def handle_request(hotels: dict, parser, search_param: dict):
                 search_param["adultNum"],
             )
             url = hotel.url
-            print(url)
-            task = asyncio.create_task(get_hotel(session, url, parser, hotel))
+            task = asyncio.create_task(get_hotel(session, url, parser, hotel, sem))
             results.append(task)
 
         return await asyncio.gather(*results)
